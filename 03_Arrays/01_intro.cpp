c60f066261a5fbd903240f09c45d01a011c97878
CodeByMehra <kpvishalmehra@gmail.com>
2025-08-31 18:08:01 +0530

    Some improvemnts in intro

--- 03_Arrays/01_intro.cpp
+++ 03_Arrays/01_intro.cpp
@@ -7,12 +7,16 @@ int main() {
     // Declare an array of integers
     int arr[5];
 
+    //checking size of array
+    int size= sizeof(arr)/sizeof(arr[0]);
+    cout<<"Size : "<<size<<endl;
+
     //taking input using loop
-    for (int i=0; i<=5; i++){
+    for (int i=0; i<5; i++){
         cin>>arr[i];
     }
-
-    for (int i=0; i<=5; i++){
-        cout<<arr[i];
+    //Output using loop
+    for (int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
     }
 }
