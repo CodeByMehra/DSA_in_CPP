1bace8b3eedb0c51fea85d9ff3f09e0329fce82f
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-02 17:50:38 +0530

    Tree structure comments added in fibonacci or better understanding

--- 05_Recursion/04_PQ3.cpp
+++ 05_Recursion/04_PQ3.cpp
@@ -12,12 +12,8 @@ int main(){
     cout<<fibonacci(4);
 }
 
-// fibo(4)
-// ├── fibo(3)
-// │   ├── fibo(2)
-// │   │   ├── fibo(1)
-// │   │   └── fibo(0)
-// │   └── fibo(1)
-// └── fibo(2)
-//     ├── fibo(1)
-//     └── fibo(0)
+    //             fibo(4)
+    //            /      \
+    //       fibo(3)    fibo(2)
+    //       /    \
+    //  fibo(2)  fibo(1)
