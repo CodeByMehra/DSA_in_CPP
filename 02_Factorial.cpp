fd8bb019b51db2dbf6d33c21af7723a108407393
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-02 16:52:13 +0530

    Improved factorial

--- 02_Factorial.cpp
+++ 02_Factorial.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int factorial(int num){
-    if (num<=1) return num;
-    num=num*factorial(num-1);
+    if (num==1 || num==0) return 1;
+    return num*factorial(num-1);
 };
 
 int main(){
