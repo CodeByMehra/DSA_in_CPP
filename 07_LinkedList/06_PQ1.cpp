122108c95f6a13e5d42063b076df004454879f02
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-05 15:10:41 +0530

    PQ1 solved

--- 07_LinkedList/06_PQ1.cpp
+++ 07_LinkedList/06_PQ1.cpp
@@ -24,9 +24,29 @@ class linkedList{
         size=0;
     }
 
-    
+    void insert(int value){
+        node* temp= new node(value);
+        if(size==0){
+            head=tail=temp;
+        }else{
+            tail->next=temp;
+            tail=temp;
+        }
+        size++;
+    }
+
+    void display(){
+        node* temp = head;
+        for(int i=0; i<size; i++){
+            cout<<temp->value<<" ";
+            temp= temp->next;
+        }
+        cout<<endl<<"Size : "<<size;
+    }
 };
 
 int main(){
-
+    linkedList li;
+    li.insert(10);
+    li.display();
 }
