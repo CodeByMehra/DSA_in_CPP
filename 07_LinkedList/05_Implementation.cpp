426dfc8a4cda8bcdb247b177a029e7cd80d58c31
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-05 12:03:02 +0530

    Get at index function implemented

--- 07_LinkedList/05_Implementation.cpp
+++ 07_LinkedList/05_Implementation.cpp
@@ -1,62 +1,115 @@
-//implementation o Linked List Class
+// implementation o Linked List Class
 
 #include <iostream>
 using namespace std;
 
-class node{
-    public:
+class node
+{
+public:
     int value;
-    node* next; 
-    node(int value){
+    node *next;
+    node(int value)
+    {
         this->value = value;
         this->next = NULL;
     }
 };
 
-class linkedList{
-    public:
-    node* head;
-    node* tail;
+class linkedList
+{
+public:
+    node *head;
+    node *tail;
     int size;
 
-    linkedList(){
-        head= NULL;
-        tail=NULL;
-        size=0;
+    linkedList()
+    {
+        head = NULL;
+        tail = NULL;
+        size = 0;
     }
 
-    void insertAtBegin(int value){
-        node* temp= new node(value);
-        if(size==0){
-        head=tail=temp;
-    }else{
-        temp->next = head;
-        head = temp;
-    }
-    size++;
+    void insertAtBegin(int value)
+    {
+        node *temp = new node(value);
+        if (size == 0)
+        {
+            head = tail = temp;
+        }
+        else
+        {
+            temp->next = head;
+            head = temp;
+        }
+        size++;
     }
 
-    void insertAtEnd(int value){
-    node* temp= new node(value);
-    if(size==0){
-        head=tail=temp;
-    }else{
-        tail->next = temp;
-        tail = temp;
+    void insertAtEnd(int value)
+    {
+        node *temp = new node(value);
+        if (size == 0)
+        {
+            head = tail = temp;
+        }
+        else
+        {
+            tail->next = temp;
+            tail = temp;
+        }
+        size++;
     }
-    size++;
-}
-    void display(){
-    node* temp=head;
-    while( temp!=NULL){
-        cout<<temp->value<<" ";
-        temp = temp->next;
+    void insertAtIdx(int idx, int value)
+    {
+        if (idx < 0 || idx > size)
+        {
+            cout << " Invalid Index " << endl;
+        }
+        else if (idx == 0)
+        {
+            insertAtBegin(value);
+        }
+        else if (idx == size)
+        {
+            insertAtEnd(value);
+        }
+        else
+        {
+            node *temp = new node(value);
+            node *t = head;
+            for (int i = 1; i < idx - 1; i++)
+            {
+                t = t->next;
+            }
+            temp->next = t->next;
+            t->next = temp;
+        }
     }
-    cout<<endl;
-}
 
+    int getAtIdx(int idx)
+    {
+        if (idx < 0 || idx >= size)
+        {
+            cout << "Invalid Index" << endl;
+            return -1; // indicate error
+        }
+        node *temp = head;
+        for (int i = 0; i < idx; i++)
+        {
+            temp = temp->next;
+        }
+        return temp->value;
+    }
+    void display()
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
-
 
 int main(){
     linkedList list1;
@@ -65,5 +118,9 @@ int main(){
     list1.insertAtEnd(20);
     list1.display();
     list1.insertAtBegin(4);
+    list1.display();
+
+    list1.getAtIdx(2);
+    list1.insertAtIdx(2,5);
     list1.display();
 }
