9680afc5a1de5d73f134eb1d925ba91510e71ed2
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-06 16:30:34 +0530

    PQ 3 of linked list solved

---
+++ 07_LinkedList/08_PQ3.cpp
@@ -0,0 +1,77 @@
+// Q: Remove Nth Node From End of List
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int value;
+    node* next;
+    node(int value){
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+class linkedList{
+    public:
+    node* head;
+    node* tail;
+    int size;
+
+    linkedList(){
+        head = tail = NULL;
+        size=0;
+    }
+
+    void insert(int value){
+        node* temp= new node(value);
+        if(size==0){
+            head=tail=temp;
+        }else{
+            tail->next=temp;
+            tail=temp;
+        }
+        size++;
+    }
+
+    void deleteFromEnd(int n){
+        node* slow = head;
+        node* fast = head;
+
+        for(int i=0; i<n; i++){
+            fast=fast->next;
+        }
+
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+
+        slow->next=slow->next->next;
+
+    }
+    
+
+    void display(){
+        node* temp = head;
+        for(int i=0; i<size; i++){
+            cout<<temp->value<<" ";
+            temp= temp->next;
+        }
+        cout<<endl<<"Size : "<<size<<endl;
+    }
+};
+
+int main(){
+    linkedList li;
+    li.insert(10);
+    li.insert(20);
+    li.insert(30);
+    li.insert(40);
+    li.insert(50);
+    li.display();
+    li.deleteFromEnd(3);
+    li.display();
+}
