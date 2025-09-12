33b07a6b26b9430d350107cdd5b0333c56f6da17
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-12 17:24:43 +0530

    PQ1solved

--- 09_Binary Trees/04_PQ1(LC).cpp
+++ 09_Binary Trees/04_PQ1(LC).cpp
@@ -1,43 +1,21 @@
-//Q: Diameter of a binary tree [543]
-#include <iostream>
-#include<climits>
-using namespace std;
-
-class TreeNode{
-    public:
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-
-    TreeNode(int val){
-        this->val = val;
-        this->left = NULL;
-        this->right= NULL;
-    }
-};
-
-void display(TreeNode* root){
-    if(root== NULL) return;
-    cout<<root->val<<" ";
-    display(root->left);
-    display(root->right);
-}
+// //Q: Diameter of a binary tree [543]
 
 
-int main(){
-    TreeNode* b = new TreeNode(2);
-    TreeNode* a = new TreeNode(1);
-    TreeNode* c = new TreeNode(3);
-    TreeNode* d = new TreeNode(4);
-    TreeNode* e = new TreeNode(5);
-    TreeNode* f = new TreeNode(6);
-    TreeNode* g = new TreeNode(7);
+//     int maxDia = 0;
 
-    a->left = b;
-    a->right= c;
-    b->left = d;
-    b->right= e;
-    c->left = f;
-    c->right= g;
-    display(a);
-}
+//     int levels(TreeNode* root){
+//         if(root == NULL) return 0;
+//         return 1 + max(levels(root->left), levels(root->right));
+//     }
+
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         if(root == NULL) return 0;
+//         int dia = levels(root->left) + levels(root->right);
+//         maxDia = max(maxDia, dia);
+//         diameterOfBinaryTree(root->left);
+//         diameterOfBinaryTree(root->right);
+//         return maxDia;
+//     }
+
+
+
