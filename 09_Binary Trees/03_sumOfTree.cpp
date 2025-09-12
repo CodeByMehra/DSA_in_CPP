39db01da2a5f02469e03b5edc6ad0d3aa58037de
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-12 16:30:53 +0530

    Diplay funtion and sum calculation function implemented

---
+++ 09_Binary Trees/03_sumOfTree.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node* left;
+    node* right;
+
+    node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right= NULL;
+    }
+};
+
+void display(node* root){
+    if(root== NULL) return;
+    cout<<root->val<<" ";
+    display(root->left);
+    display(root->right);
+}
+int sumOfTree(node* root){
+    if(root== NULL) return 0;
+    int sum=0;
+    sum= root->val+sumOfTree(root->left)+sumOfTree(root->right);
+    return sum;
+}
+
+int main(){
+    node* a = new node(1);
+    node* b = new node(2);
+    node* c = new node(3);
+    node* d = new node(4);
+    node* e = new node(5);
+    node* f = new node(6);
+    node* g = new node(7);
+
+    a->left = b;
+    a->right= c;
+    b->left = d;
+    b->right= e;
+    c->left = f;
+    c->right= g;
+    display(a);
+    cout<<sumOfTree(a);
+}
