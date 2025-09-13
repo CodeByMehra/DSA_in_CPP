f5e7012fe2679c73685a1dfa0c8f41b83c1b2389
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-13 16:45:16 +0530

    PQ 2 commented

--- 09_Binary Trees/05_PQ2(LC).cpp
+++ 09_Binary Trees/05_PQ2(LC).cpp
@@ -1,19 +1,19 @@
-//Compare if the given 2 trees are same or not? [100]
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) return true;
-        if (p == NULL && q != NULL) return false;
-        if (p != NULL && q == NULL) return false;
+// //Compare if the given 2 trees are same or not? [100]
+// class Solution {
+// public:
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         if (p == NULL && q == NULL) return true;
+//         if (p == NULL && q != NULL) return false;
+//         if (p != NULL && q == NULL) return false;
 
-        if (p->val != q->val) return false;
+//         if (p->val != q->val) return false;
 
-        bool leftAns = isSameTree(p->left, q->left);
-        if (leftAns == false) return false;
+//         bool leftAns = isSameTree(p->left, q->left);
+//         if (leftAns == false) return false;
 
-        bool rightAns = isSameTree(p->right, q->right);
-        if (rightAns == false) return false;
+//         bool rightAns = isSameTree(p->right, q->right);
+//         if (rightAns == false) return false;
 
-        return true;
-    }
-};
+//         return true;
+//     }
+// };
