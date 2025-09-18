d53774d0a2974f1794c550a70136a085c1d3668f
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-18 15:45:34 +0530

    Q2 done

--- 10_BST/PQ02(LC).cpp
+++ 10_BST/PQ02(LC).cpp
@@ -1,1 +1,20 @@
-//701- Insert into a binary search tree
+// //701- Insert into a binary search tree
+// class Solution {
+// public:
+//     TreeNode* insertIntoBST(TreeNode* root, int val) {
+//         if(root==NULL) return new TreeNode(val);
+//         else if(root->val > val) { // go left
+//             if(root->left == NULL) { // attach it here
+//                 root->left = new TreeNode(val);
+//             }
+//             else insertIntoBST(root->left, val);
+//         }
+//         else { // root->val < val : go right
+//             if(root->right == NULL) { // attach it here
+//                 root->right = new TreeNode(val);
+//             }
+//             else insertIntoBST(root->right, val);
+//         }
+//         return root;
+//     }
+// };
