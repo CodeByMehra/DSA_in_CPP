c274c3e46eeab3a779978d381139edfe2f0cf491
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-03 15:40:22 +0530

    practice q on array

--- 03_Arrays/02_PQ1.cpp
+++ 03_Arrays/02_PQ1.cpp
@@ -1,22 +1,3 @@
-//Q: Calculate the sum of all the elements in the given array
+//Q: Given an array of marks of students, if the marks of any student is less than 35, print its roll number. [roll number here refer to the index]
 
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cout<<"Enter size of the array : ";
-    cin>>n;
-    int arr[n];
 
-    for(int i=0; i<n; i++){
-        cout<<"Enter value of "<<n<<" Elements"<<endl;
-        cin>>arr[i];
-        };
-
-    int sum=0;
-        for(int i=0; i<n; i++){
-            sum= sum+ arr[i];
-        }
-    cout<<"Sum : "<<sum;
-    return 0;
-}
