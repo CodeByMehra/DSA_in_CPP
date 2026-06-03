ee74571a0412c4862d2d13ecc1d13fc27f532ce0
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-03 15:47:38 +0530

    array Q1 solved

--- 03_Arrays/02_PQ1.cpp
+++ 03_Arrays/02_PQ1.cpp
@@ -1,3 +1,20 @@
 //Q: Given an array of marks of students, if the marks of any student is less than 35, print its roll number. [roll number here refer to the index]
 
 
+#include <iostream>
+using namespace std;
+int main(){
+
+    int arr[] = {90,30,32,85,34,97};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i=0; i<size; i++){
+        int marks = arr[i];
+        if (marks < 35){
+            cout<<"Roll number "<<i<<" Has marks less than 35"<<endl;
+        }
+    }
+
+    return 0;
+}
