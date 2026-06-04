fe2042b2f0c1f86a72211dc687c66fa6becf447b
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-04 18:59:37 +0530

    wrong code

--- 03_Arrays/00_Practice/01_RotateByK.cpp
+++ 03_Arrays/00_Practice/01_RotateByK.cpp
@@ -16,17 +16,31 @@
 #include <iostream>
 using namespace std;
 
-void rotateByK(int arr[], int size)
+void display(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<< " ";
+    }
+}
+
+void rotateByK(int arr[], int size, int k)
 {
-    
+   int j=size-1;
+   int temp;
+   for(int i=0; i<k; i++){
+        temp=arr[i];
+        arr[i] = arr[j];
+        arr[j]=temp;
+
+   }
+   display(arr, size);
 }
 
 int main()
 {
     int arr[] = {1,2,3,4,5};
     int size = sizeof(arr) / sizeof(arr[0]);
-
-    rotateByK(arr, size);
+    int k = 2;
+    rotateByK(arr, size, k);
 
     return 0;
 }
