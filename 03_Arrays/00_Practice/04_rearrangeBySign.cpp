ba8aff6e2034eb56d527c96c3cdfd998b6b525e7
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-05 18:48:50 +0530

    added 5th problem statement

---
+++ 03_Arrays/00_Practice/04_rearrangeBySign.cpp
@@ -0,0 +1,20 @@
+// You are given an array containing:
+
+// Positive numbers & Negative numbers
+
+// You need to rearrange them in an alternating manner.
+// Usually:
+
+// Positive ,Negative , Positive, Negative...
+// or
+// Negative , Positive, Negative, positive...
+// depending on the question.
+// Example
+// Array:
+
+// [1, 2, -4, -5]
+// Rearranged:
+
+// [1, -4, 2, -5]
+// Pattern:
+// +  -  +  -
