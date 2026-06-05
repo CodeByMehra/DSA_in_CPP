300f27785cd8b63efefb4d29606f0a4eb8ae1d58
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-05 18:46:10 +0530

    problem statement added

---
+++ 03_Arrays/00_Practice/03_equilibrium_index.cpp
@@ -0,0 +1,24 @@
+// Find an index in the array where:
+
+// Sum of elements on the left
+// =
+// Sum of elements on the right
+
+// The element at that index itself is not included in either sum.
+// Example
+// [1, 3, 5, 2, 2]
+
+// Consider index 2:
+//       5
+//     ↑
+// Left side:
+
+// 1 + 3 = 4
+
+// Right side:
+
+// 2 + 2 = 4
+// Both sums are equal.
+
+// Therefore:
+// Equilibrium Index = 2
