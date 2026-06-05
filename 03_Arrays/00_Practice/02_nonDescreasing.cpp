55d582e7349505540049eb887a3956ee882cd2e3
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-05 18:44:23 +0530

    non decreasing array problem statement added

---
+++ 03_Arrays/00_Practice/02_nonDescreasing.cpp
@@ -0,0 +1,20 @@
+// Problem Statement:
+// You are given an array. Determine whether the array is non-decreasing.
+// Meaning of Non-Decreasing:
+// Every element should be:
+
+// Greater than the previous element, OR
+// Equal to the previous element
+// Example 
+// [1, 2, 2, 4, 5]
+
+// Check:
+
+// 2 >= 1 ✓
+// 2 >= 2 ✓
+// 4 >= 2 ✓
+// 5 >= 4 ✓
+
+// Output:
+
+// True
