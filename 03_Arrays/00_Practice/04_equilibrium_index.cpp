ec607bb2600ecad366c3ae90f121f04bad137c32
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-06 18:07:43 +0530

    added solution for problem 4

--- 03_Arrays/00_Practice/04_equilibrium_index.cpp
+++ 03_Arrays/00_Practice/04_equilibrium_index.cpp
@@ -22,3 +22,56 @@
 
 // Therefore:
 // Equilibrium Index = 2
+
+#include <iostream>
+using namespace std;
+
+// Function to calculate left sum
+int leftSum(int arr[], int index)
+{
+    int sum = 0;
+
+    for(int i = 0; i < index; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+// Function to calculate right sum
+int rightSum(int arr[], int size, int index)
+{
+    int sum = 0;
+
+    for(int i = index + 1; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int equilibriumIndex(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        if(leftSum(arr, i) == rightSum(arr, size, i))
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1, 3, 5, 2, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Equilibrium Index = "
+         << equilibriumIndex(arr, size);
+
+    return 0;
+}
