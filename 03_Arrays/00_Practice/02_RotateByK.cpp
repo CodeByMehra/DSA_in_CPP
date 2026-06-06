4ad1665e33302fe09b1f6dfe1ac60763d21e43cc
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-06 10:28:07 +0530

    rotate byk solved

--- 03_Arrays/00_Practice/02_RotateByK.cpp
+++ 03_Arrays/00_Practice/02_RotateByK.cpp
@@ -13,3 +13,38 @@
 // After rotating right by 2 positions:
 // [4, 5, 1, 2, 3]
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotateByK(vector <int> arr,int k){
+
+    vector <int> rotatedArr;
+
+    for(int i=arr.size()-k; i<arr.size(); i++){
+       rotatedArr.push_back(arr[i]);
+    }
+
+    for(int i=0; i<arr.size()-k; i++){
+       rotatedArr.push_back(arr[i]);
+    }
+
+    for (int i=0; i<rotatedArr.size(); i++){
+        cout<<rotatedArr[i]<<" ";
+    }
+}
+
+int main(){
+
+    vector <int> arr;
+    arr.push_back(1);
+    arr.push_back(2);
+    arr.push_back(3);
+    arr.push_back(4);
+    arr.push_back(5);
+    arr.push_back(6);
+
+    rotateByK(arr, 2);
+
+    return 0;
+}
