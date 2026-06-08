a7b93cec1f9ceece7c2d49b8127e75fe68e953e5
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-08 12:33:40 +0530

    added mutability

--- 05_Strings/01_intro.cpp
+++ 05_Strings/01_intro.cpp
@@ -4,13 +4,17 @@ int main(){
 
     // char str[5] = {'a','b','c','d','e'};   
 
-    string str = "Vishal"; 
-    cout<<str[0];
+    string str = "Vishol"; 
+    cout<<str<<endl;
+    str[4] = 'a';  //single character cna be updated in strings, means it is mutable in C++
+    cout<<str;
 
     string s; 
     // cin>>s; - can take input without spaces, 
     getline(cin,s); // to take input along with space
     cout<<s;  
+
+
 
     return 0;
 }
