3b89563300232556f5c9deaa661db8adb6c75013
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-09 10:01:55 +0530

    Ques:  input a string and return the number of times the neighbouring characters are different from each other

---
+++ 05_Strings/practice/PQ5.cpp
@@ -0,0 +1,28 @@
+// Ques:  input a string and return the number of times the neighbouring characters are different from each other
+// intuition = if any neighbouring character will match, count will not increased
+
+#include <iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+
+    int count=0;
+
+    for(int i=0; i<s.length(); i++){
+        if(i==0){                               //for first index
+            if(s[i]!=s[i+1]){
+                count++;
+            }  
+        }
+        else if(i==s.length()-1){                   //for last index
+            if(s[i]!=s[i-1]){
+                count++;
+            }
+        }
+        else if(s[i]!=s[i+1] && s[i]!=s[i-1]){
+            count++;
+        }
+    }
+cout<<count;
+}
---
+++ 05_Strings/practice/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+input a string and return the number of times the neighbouring characters are different from each other
+// intuition = if any neighbouring character will match, count will not increased
