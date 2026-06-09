cd14a6dc424012c774bae4db1efd22f89579c5a1
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-09 13:27:12 +0530

    determine if they are isomorphic. solved

--- 05_Strings/practice/PQ10_isomorphic.cpp
+++ 05_Strings/practice/PQ10_isomorphic.cpp
@@ -20,3 +20,47 @@
 
 // Mapping:
 // false;
+
+//Solution approach: we will create a special array of 150 size to store value of all possible values, after this, we will subtract each value of same index, if the character repeat and the value at that place is came as new calculated value, then it is isomorphic, else not
+
+//base case: if length of both not equal, then not isomorphic 
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+
+        if(s.length() != t.length())
+            return false;
+
+        vector<int> v(150, 1000);            //initialized vector with 1000 because difference value can reach this
+
+        for(int i = 0; i < s.length(); i++) {   // checking from the perspective of s
+
+            int idx = (int)s[i];
+
+            if(v[idx] == 1000)
+                v[idx] = s[i] - t[i];
+
+            else if(v[idx] != (s[i] - t[i]))
+                return false;
+        }
+
+        // resetting the vector
+
+        for(int i = 0; i < 150; i++)            
+            v[i] = 1000;
+
+        for(int i = 0; i < s.length(); i++) {      // checking from perspective of t
+
+            int idx = (int)t[i];
+
+            if(v[idx] == 1000)
+                v[idx] = t[i] - s[i];
+
+            else if(v[idx] != (t[i] - s[i]))
+                return false;
+        }
+
+        return true;
+    }
+};
