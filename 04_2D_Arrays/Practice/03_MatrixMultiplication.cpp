1c1430d9688b45a15943d7431a2d0251adc3e715
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-11 18:20:23 +0530

    DSA Done for the day

---
+++ 04_2D_Arrays/Practice/03_MatrixMultiplication.cpp
@@ -0,0 +1,10 @@
+//Ques: WAP to print the multiplication of two matrices given by user
+
+//soluyion: base rule for matrix multiplication= number of columns of M1 must be equal to number of cols in M2;
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+}
