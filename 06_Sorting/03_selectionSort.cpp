cca9404154ace52aebc7c3de661bcc69c05c8aee
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-24 16:34:23 +0530

    selection sort incomplete

---
+++ 06_Sorting/03_selectionSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void selectionSort(int arr[], int n){
+    for(int i=0; i>n; i++){
+        int min = INT_MAX;
+        if(arr[i]<min){
+            min= arr[i];
+        }
+    }
+    for(int i=0; i>n; i++){
+        
+    }
+}
+
+int main() {
+
+    int arr[] = {4, 1 ,6 , 5, 2}; 
+
+    return 0;
+}
