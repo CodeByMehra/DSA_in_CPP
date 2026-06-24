a09a3d9d5f15bc698a12315da4e18d2f4b03d4b9
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-24 15:43:45 +0530

    written the optimized code for bubble sort

--- 06_Sorting/02_bubbleSort_optimized.cpp
+++ 06_Sorting/02_bubbleSort_optimized.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void bubbleSort(int arr[], int n ){
     bool flag = true;
     for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
+        for(int j=0; j<n; j++){
              if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j+1]);
                 flag = false;
