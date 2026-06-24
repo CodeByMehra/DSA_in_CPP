55876257f68d2c564b73bf2a44490527f57ea0b2
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-24 15:43:11 +0530

    debugged the bubble sort/

--- 06_Sorting/02_bubbleSort_optimized.cpp
+++ 06_Sorting/02_bubbleSort_optimized.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 void bubbleSort(int arr[], int n ){
+    bool flag = true;
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+        for(int j=i+1; j<n; j++){
              if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                swap(arr[j], arr[j+1]);
+                flag = false;
               }    
+        }
+        if(flag==true){
+            break;
         }
     }
 
