9d6fb8744cd053c4413ce90409658b8df0572cfe
CodeByMehra <kpvishalmehra@gmail.com>
2026-07-23 12:23:10 +0530

    solved maze problem

--- 08_Recursion/06_PQ6.cpp
+++ 08_Recursion/06_PQ6.cpp
@@ -1,9 +1,22 @@
+// Maze path : 
+
 #include <iostream>
 using namespace std;
 
-int main() {
+// Taking starting row, starting column, ending row, ending column as parameters
+int maze(int sr, int sc, int er, int ec){
+    if(sr> er || sc>ec) return 0;
+    if (sr==er && sc==ec) return 1;
+    int rightways = maze(sr, sc+1, er, ec);
+    int downways = maze(sr+1, sc, er, ec);
+    int totalways = rightways + downways;
+    return totalways;
 
-    
+}
 
-    return 0;
+int main(){
+    int n;
+    cout<<"Enter no. of stairs : ";
+    cin>>n;
+    cout<<"Possible paths : "<<maze(0,0,2,2);
 }
