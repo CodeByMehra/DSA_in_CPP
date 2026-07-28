3b07981b50dec0316b6279680c2d7febed8294e6
CodeByMehra <kpvishalmehra@gmail.com>
2026-07-28 11:20:55 +0530

    practicing DSa Linked list

---
+++ 10_LinkedList/06_PQ1_countNodes.cpp
@@ -0,0 +1,16 @@
+//Q:Write a function countNodes() that counts how many nodes are present in the list.
+
+#include <iostream>
+using namespace std;
+
+class node{
+    int value;
+};
+
+
+int main() {
+
+    
+
+    return 0;
+}
