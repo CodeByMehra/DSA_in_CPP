e7acf16ad24fec571dc8a3fa52e22fc2ede60f4b
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-01 14:00:57 +0530

    added the best approach

--- 10_LinkedList/13_PQ8.cpp
+++ 10_LinkedList/13_PQ8.cpp
@@ -35,3 +35,37 @@
 //         return c->next;
 //     }
 // };
+
+
+
+
+
+// Best approact : In this approach, we are not using any extra space. We simply just created a temporary node and connected its next by comparing the nodes of the, both of the lists. It will do the modifications in the lists itself.
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        
+//         ListNode* c = new ListNode(100);
+//         ListNode* temp = c;
+
+//         while(a!=NULL && b!=NULL){
+//             if(a->val<=b->val){
+//                 temp->next  = a;
+//                 a = a->next;
+//                 temp = temp->next;
+//             }
+//             else{
+//                 temp->next  = b;
+//                 b = b->next;
+//                 temp = temp->next;
+//             }
+//         }
+//         if(a==NULL){
+//             temp->next = b;
+//         }
+//         else{
+//             temp->next=a;
+//         }
+//         return c->next;
+//     }
+// };
