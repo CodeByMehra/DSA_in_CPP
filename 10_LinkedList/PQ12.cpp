16119226ef44bad398e77f33866f89ec1e62f1ad
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-04 13:39:43 +0530

    added the solution

--- 10_LinkedList/PQ12.cpp
+++ 10_LinkedList/PQ12.cpp
@@ -1,1 +1,18 @@
-// Q: Reverse Linked List  (LC206- Easy)
+// Q: 206. Reverse Linked List
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* Prev = NULL;
+//         ListNode* Current = head;
+//         ListNode* Next = NULL;
+
+//         while(Current){
+//             Next = Current->next;
+//             Current->next = Prev;
+//             Prev = Current;
+//             Current = Next;
+//         }
+//         return Prev;
+//     }
+// };
