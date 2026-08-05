5af91b7ef168272e708bd5354d57426354ad1614
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-05 10:15:55 +0530

    solved palindrome lisnked list

--- 10_LinkedList/18_PQ13.cpp
+++ 10_LinkedList/18_PQ13.cpp
@@ -1,2 +1,40 @@
 // Q 234 med : Palindrome Linked List 
 
+// class Solution {
+// public:
+//         ListNode* reverseList(ListNode* head) {
+//         ListNode* Prev = NULL;
+//         ListNode* Current = head;
+//         ListNode* Next = NULL;
+
+//         while(Current){
+//             Next = Current->next;
+//             Current->next = Prev;
+//             Prev = Current;
+//             Current = Next;
+//         }
+//         return Prev;
+//     }
+//     bool isPalindrome(ListNode* head) {
+//         ListNode* c = new ListNode(0);
+//         // copying the lisnked list
+//         ListNode* temp = head;
+//         ListNode* tempC = c;
+//         while(temp){
+//             ListNode* node = new ListNode(temp->val);
+//             tempC->next = node;
+//             temp = temp->next;
+//             tempC = tempC->next;
+//         }
+//         c = c->next;
+//         c = reverseList(c);
+//         ListNode* a = head;
+//         ListNode* b = c;
+//         while(a->next!=NULL || b->next!=NULL){
+//             if(a->val!=b->val) return false;
+//             a = a->next;
+//             b = b->next;
+//         }
+//         return true;
+//     }
+// };
