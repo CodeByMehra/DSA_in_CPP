370c5f8b6bd06bcea035615a79c7f854225fc08b
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-05 09:45:31 +0530

    PQ 12 solved by recursion

--- 10_LinkedList/PQ12.cpp
+++ 10_LinkedList/PQ12.cpp
@@ -16,3 +16,15 @@
 //         return Prev;
 //     }
 // };
+
+// Recursive Solution:
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(head==NULL || head->next==NULL) return head;
+//         ListNode* newHead = reverseList(head->next);
+//         head->next->next = head;
+//         head->next = NULL;
+//         return newHead;
+//     }
+// };
