fcc89b916a43a300d83b552878715c3bdf3831ea
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-06 13:29:37 +0530

    implementation done

--- 10_LinkedList/02_Doubly_LL/01_implementation.cpp
+++ 10_LinkedList/02_Doubly_LL/01_implementation.cpp
@@ -15,8 +15,22 @@ class Node{
 };
 
 int main() {
-
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    Node* e = new Node(50);
     
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+
+    e->prev = d;
+    d->prev = c;
+    c->prev = b;
+    b->prev = a;
+   
 
     return 0;
 }
