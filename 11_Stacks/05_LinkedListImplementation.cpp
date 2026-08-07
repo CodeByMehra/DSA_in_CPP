a0d6d873c5ae750ee9ff11751506f4f4c213b706
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-07 13:34:44 +0530

    implementation done

--- 11_Stacks/05_LinkedListImplementation.cpp
+++ 11_Stacks/05_LinkedListImplementation.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 using namespace std;
 
+class node{
+    public:
+    int val;
+    node* next;
+
+    node(int value){
+        this->val= value;
+        this->next = NULL;
+
+    }
+};
+
+class stack{
+    public:
+    node*head;
+    int size;
+    stack(){
+        head=NULL;
+        size=0;
+    }
+
+    void push(int val){
+        node* temp = new node(val);
+        temp->next = head;
+        head = temp;
+        size++;
+    }
+
+    void pop(){
+        if(head==NULL) cout<<"stack is empty";
+        head = head->next;
+        size--;
+    }
+
+    void top(){
+        if(head==NULL) cout<<"stack is empty";
+        cout<<head->val;
+    }
+};
 int main() {
 
     
