d42ee57af082ebe6f8404b9927795f06ef1b2137
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-08 09:49:49 +0530

    written the intuition

---
+++ 11_Stacks/06_PQ1.cpp
@@ -0,0 +1,11 @@
+// Q : Balanced Brackets
+
+
+
+//  Steps:
+//  1. if you see an opening bracket:
+//         push
+//  2. if you see a closing bracket:
+//         Top ko dekho: if there is opening bracket , pop it
+//  3. at the end :
+//         if stack size == 0, return true else false
