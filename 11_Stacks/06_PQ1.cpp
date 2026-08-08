d53fd02541b91851adcb6d59489919e390319224
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-08 10:07:33 +0530

    completed the question

--- 11_Stacks/06_PQ1.cpp
+++ 11_Stacks/06_PQ1.cpp
@@ -1,6 +1,33 @@
 // Q : Balanced Brackets
 
+#include <iostream>
+#include <stack>
+using namespace std;
 
+bool checkBalance(string str){  
+    if (str.length()%2 !=0) return false;
+    stack <char> st;
+
+    for(int i=0; i<str.length(); i++){
+        if(str[i]=='('){
+            st.push(str[i]);
+        }
+        else if(str[i] == ')' && st.size()>0){
+            st.pop();
+        }
+    }
+    if(st.size()==0) return true;
+    else return false;
+
+}
+
+int main() {
+    string s = ")(())";
+    cout<<checkBalance(s);
+    
+
+    return 0;
+}
 
 //  Steps:
 //  1. if you see an opening bracket:
