97beffb2ed87f60e2e8b41e373f44f84025c6c05
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-08 10:29:43 +0530

    completed the question

--- 11_Stacks/07_PQ2.cpp
+++ 11_Stacks/07_PQ2.cpp
@@ -12,3 +12,34 @@
 // Output:
 // abcd
 
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+void consecutive(string str){
+    stack <char> st;
+
+    for(int i=0; i<str.length(); i++){
+        if (st.size()==0) st.push(str[i]);
+
+        if(st.top() != str[i]) st.push(str[i]);
+    }
+
+    string newStr;
+    while(st.size()!=0){
+        newStr.push_back(st.top());
+        st.pop();
+    }
+    reverse(newStr.begin(), newStr.end());
+    cout<<newStr;
+
+}
+
+int main() {
+    string s = "aaaaabbbbbccccccdijjj";
+    consecutive(s);
+    
+
+    return 0;
+}
